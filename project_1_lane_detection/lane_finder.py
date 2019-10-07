c17ce4d7c2a1c8234183eeaf5363d60a00aa7534
劉翼傑 <jjaacckkyy63@gmail.com>
2019-10-07 11:03:30 -0400

    advanced method

--- project_1_lane_detection/lane_finder.py
+++ project_1_lane_detection/lane_finder.py
@@ -8,7 +8,7 @@ def preprocessing(img):
     if len(img.shape) > 2: 
         #convert into hsv
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # white bound
         w_lower_bound = np.array([0,0,0], dtype=np.uint8)
         w_upper_bound = np.array([0,0,255], dtype=np.uint8)
@@ -20,10 +20,10 @@ def preprocessing(img):
         w_mask = cv2.inRange(hsv, w_lower_bound, w_upper_bound)
         y_mask = cv2.inRange(hsv, y_lower_bound, y_upper_bound)
 
-        w_res = cv2.bitwise_and(img, img, mask=w_mask)
-        y_res = cv2.bitwise_and(img, img, mask=y_mask)
+        w_res = cv2.bitwise_and(gray, gray, mask=w_mask)
+        y_res = cv2.bitwise_and(gray, gray, mask=y_mask)
 
-        img = img + w_res + y_res
+        img = gray + w_res + y_res
 
         return img
 
@@ -36,12 +36,28 @@ def hough_lines_detection(img, rho, theta, threshold, min_line_len, max_line_gap
     return lines
 
 
+#def detect_lines
+
+
 if __name__ == "__main__":
-    test_image = sys.argv[1]
-    i1, i2 = preprocessing(cv2.imread(test_image))
-    cv2.imshow("i1", i1)
+
+    img_gray = cv2.cvtColor(cv2.imread(sys.argv[1]), cv2.COLOR_BGR2GRAY)
+    print(img_gray.shape)
+    # perform gaussian blur
+    img_blur = cv2.GaussianBlur(img_gray, (17, 17), 0)
+    
+
+    # perform edge detection
+    img_edge = cv2.Canny(img_blur, threshold1=50, threshold2=80)
+
+    cv2.imshow('image',img_edge)
     cv2.waitKey(0)
-    cv2.destroyWindow('i1')
-    cv2.imshow("i2", i2)
-    cv2.waitKey(0)
-    cv2.destroyWindow('i2')
+    cv2.destroyAllWindows()
+
+    # perform hough transform
+    detected_lines = hough_lines_detection(img=img_edge,
+                                           rho=2,
+                                           theta=np.pi / 180,
+                                           threshold=1,
+                                           min_line_len=15,
+                                           max_line_gap=5)
